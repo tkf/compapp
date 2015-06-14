6f453991c76471e781aefafcafb06946e2d30db4
Takafumi Arakaki <aka.tkf@gmail.com>
2015-06-14 18:19:11 +0200

    Use sphinx.ext.todo

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -38,6 +38,7 @@ extensions = [
     'sphinx.ext.inheritance_diagram',
     'sphinx.ext.viewcode',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -110,7 +111,7 @@ pygments_style = 'sphinx'
 #keep_warnings = False
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
-todo_include_todos = False
+todo_include_todos = True
 
 
 # -- Options for HTML output ----------------------------------------------
