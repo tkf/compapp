4322d0cf7df31dc2a1ef2680876b570c76fb9489
Takafumi Arakaki <aka.tkf@gmail.com>
2016-05-04 19:43:42 +0200

    Add imports to compapp/__init__.py

--- compapp/__init__.py
+++ compapp/__init__.py
@@ -4,3 +4,10 @@
 __version__ = '0.0.0'
 __author__ = 'Takafumi Arakaki'
 __license__ = None
+
+# FIXME: no stars!
+from .core import *
+from .executables import *
+from .descriptors import *
+from .plugins import *
+from .apps import *
