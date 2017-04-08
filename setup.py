900f826d5f4a4654fb27ba61329e7a1a1622f321
Takafumi Arakaki <aka.tkf@gmail.com>
2017-04-08 03:31:55 -0700

    Update setup.py metadata

--- setup.py
+++ setup.py
@@ -7,13 +7,14 @@ setup(
     package_dir={'': 'src'},
     author='Takafumi Arakaki',
     author_email='aka.tkf@gmail.com',
-    # url='https://github.com/tkf/compapp',
-    # license=,
-    # description='compapp - THIS DOES WHAT',
-    long_description=open("README.rst").read(),
-    # keywords='KEYWORD, KEYWORD, KEYWORD',
+    url='https://github.com/tkf/compapp',
+    license='BSD-2-Clause',  # SPDX short identifier
+    description='A framework for simulations and data analysis',
+    long_description=open('README.rst').read(),
+    keywords='numerical simulation, research',
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: BSD License',
         # see: http://pypi.python.org/pypi?%3Aaction=list_classifiers
     ],
     install_requires=[
